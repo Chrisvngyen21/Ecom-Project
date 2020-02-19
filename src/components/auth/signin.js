c5be5705f8f3318f002c3f14729e51288d068ff5
Chrisman <chrisvngyen@gmail.com>
2020-02-18 21:21:58 -0800

    create page title and style

--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 
 import SignInForm from "./signinform";
+import PageTitle from "../pagetitle";
 
 class SignIn extends Component {
   onSubmit = feilds => {
@@ -10,6 +11,7 @@ class SignIn extends Component {
   render() {
     return (
       <div className="sign-in">
+        <PageTitle className="sign-in-page-title" title="Login" />
         <SignInForm onSubmit={this.onSubmit} className="sign-in-form" />
       </div>
     );
