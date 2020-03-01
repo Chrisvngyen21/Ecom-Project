c445c57b70c87b9dbe5f7c7c9d460eb86a504bb8
Chrisman <chrisvngyen@gmail.com>
2020-02-29 18:28:52 -0800

    Made sign up form and modified back button

--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -1,8 +1,20 @@
 import React, { Component } from "react";
 
+import SignUpForm from "./signupform";
+import PageTitle from "../pagetitle";
+
 class SignUp extends Component {
+  onSubmit = feilds => {
+    console.log(feilds);
+  };
+
   render() {
-    return <div className="sign-up">sign up</div>;
+    return (
+      <div className="sign-up">
+        <PageTitle className="sign-up-page-title" title="Register" />
+        <SignUpForm onSubmit={this.onSubmit} className="sign-in-form" />
+      </div>
+    );
   }
 }
 
