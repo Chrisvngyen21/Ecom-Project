650ef73e2adaed5bcfe4515940739df872e4cd4c
Chrisman <chrisvngyen@gmail.com>
2020-03-02 21:53:01 -0800

    styled header and navbar

--- src/reducers/headernavbarreducer.js
+++ src/reducers/headernavbarreducer.js
@@ -13,11 +13,13 @@ const INITIAL_STATE = {
 
   navbarLinks: [
     {
-      title: "account"
+      title: "account",
+      active: true
     },
 
     {
-      title: "purchases"
+      title: "purchases",
+      active: false
     }
   ]
 };
