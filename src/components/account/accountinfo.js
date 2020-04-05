931da47dd5309e328f6a2e9be80071ec5b685973
Chrisman <chrisvngyen@gmail.com>
2020-04-04 19:36:22 -0700

    set up account info page

--- src/components/account/accountinfo.js
+++ src/components/account/accountinfo.js
@@ -1,8 +1,27 @@
 import React, { Component } from "react";
 
+import PageTitle from "../pagetitle";
+import AccountInformationForm from "./accountInformationForm";
+
 class AccountInfo extends Component {
+  onSubmit = feilds => {
+    console.log(feilds);
+  };
+
   render() {
-    return <div>account info</div>;
+    return (
+      <div className="account-information">
+        <PageTitle
+          className="account-information"
+          title="Account Information"
+        />
+
+        <AccountInformationForm
+          onSubmit={this.onSubmit}
+          className="account-information_form"
+        />
+      </div>
+    );
   }
 }
 
