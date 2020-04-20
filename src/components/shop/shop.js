bc084e64c934b065e11533823985b7588e24f7f7
Chrisman <chrisvngyen@gmail.com>
2020-04-19 18:04:37 -0700

    filter products

--- src/components/shop/shop.js
+++ src/components/shop/shop.js
@@ -27,14 +27,33 @@ class Shop extends Component {
   }
 
   render() {
-    return <div className="shop">Shop</div>;
+    return (
+      <div className="shop">
+        {/* shop search bar */}
+        <div className="shop_products">
+          {this.props.filteredProducts.map((product) => {
+            return (
+              <div key={product._id} className="shop-product">
+                <div className="shop-product_title">{product.title}</div>
+
+                <div className="shop-product_description">
+                  {product.description}
+                </div>
+              </div>
+            );
+          })}
+        </div>
+        {/* shop cart button */}
+      </div>
+    );
   }
 }
 
 function mapStateToProps(state) {
-  const { categories } = state.shop;
+  const { categories, filteredProducts } = state.shop;
   return {
     categories,
+    filteredProducts,
   };
 }
 
--- src/reducers/shopreducer.js
+++ src/reducers/shopreducer.js
@@ -6,7 +6,7 @@ import {
 
 const INITIAL_STATE = {
   categories: [],
-  productsSelected: [],
+  filteredProducts: [],
   products: [],
 };
 export default function (state = INITIAL_STATE, action) {
@@ -25,10 +25,15 @@ export default function (state = INITIAL_STATE, action) {
       };
 
     case FILTER_PRODUCTS_WITH_CATEGORY_ID:
-      console.log(action.payload);
+      var filteredProducts = [];
+      state.products.map((product) => {
+        if (product.belongsTo.includes(action.payload)) {
+          filteredProducts.push(product);
+        }
+      });
       return {
         ...state,
-        // selectedCategoryId
+        filteredProducts,
       };
     default:
       return state;
